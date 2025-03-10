16000a0a1cee1116eef64672ccb355b5f0eeb2af
duncan britt <dbru997@gmail.com>
2025-03-10 16:15:31 -0600

    update to new version of fontaine-org

--- fontaine-org.el
+++ fontaine-org.el
@@ -34,26 +34,41 @@
   (setq fontaine-faces
         (append fontaine-faces fontaine-org-heading-faces)))
 
+(defun fontaine-org--get-face-spec (face family weight slant height width)
+  "Create a face spec for FACE with the given attributes."
+  (when (or family weight slant height width)
+    `(,face
+      ((((type graphic))
+        ,@(when family (list :family family))
+        ,@(when weight (list :weight weight))
+        ,@(when slant (list :slant slant))
+        ,@(when height (list :height height))
+        ,@(when width (list :width width)))))))
+
 (defun fontaine-org--apply-heading-fonts (preset)
   "Apply org heading fonts from PRESET configuration."
   (let* ((props (fontaine--get-preset-properties preset))
-         (variable-pitch-family (plist-get props :variable-pitch-family)))
+         (variable-pitch-family (plist-get props :variable-pitch-family))
+         (custom--inhibit-theme-enable nil)
+         (faces nil))
     (dolist (face fontaine-org-heading-faces)
       (let* ((face-name (symbol-name face))
              (family-prop (intern (format ":%s-family" face-name)))
              (weight-prop (intern (format ":%s-weight" face-name)))
              (height-prop (intern (format ":%s-height" face-name)))
              (slant-prop (intern (format ":%s-slant" face-name)))
-             (width-prop (intern (format ":%s-width" face-name))))
-        ;; If no specific family is set for this org heading level,
-        ;; use the variable-pitch family
-        (fontaine--set-face-attributes
-         face
-         (or (plist-get props family-prop) variable-pitch-family)
-         (plist-get props weight-prop)
-         (plist-get props slant-prop)
-         (plist-get props height-prop)
-         (plist-get props width-prop))))))
+             (width-prop (intern (format ":%s-width" face-name)))
+             (family (or (plist-get props family-prop) variable-pitch-family))
+             (weight (plist-get props weight-prop))
+             (slant (plist-get props slant-prop))
+             (height (plist-get props height-prop))
+             (width (plist-get props width-prop))
+             (face-spec (fontaine-org--get-face-spec 
+                         face family weight slant height width)))
+        (when face-spec
+          (push face-spec faces))))
+    (when faces
+      (apply 'custom-theme-set-faces 'fontaine faces))))
 
 (defun fontaine-org--advice-after-set-preset (&rest _)
   "Additional font settings for org-mode after fontaine preset is applied."
